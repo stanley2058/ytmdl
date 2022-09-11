b657ef0f6ffd6d85b823b56e6b1346bb1086834d
stanley2058 <stanley2058@yahoo.com.tw>
2022-09-11 20:17:33 +0800

    feat: support windows style download path

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 var downloadPath string
 
 func main() {
-	homePath := os.Getenv("HOME")
-	downloadPath = homePath + "/Downloads"
+	downloadPath = os.Getenv("XDG_DOWNLOAD_DIR")
+	if downloadPath == "" {
+		homePath, err := os.UserHomeDir()
+		if err == nil {
+			downloadPath = fmt.Sprintf("%s%cDownloads", homePath, os.PathSeparator)
+		}
+	}
 
 	createTui()
 }
